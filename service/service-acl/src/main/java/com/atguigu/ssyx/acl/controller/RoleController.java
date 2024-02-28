b3a6ce3b500e12eff95f3d4452b32f67c178ed39
Jiamiao <1737781461@qq.com>
2024-02-28 18:25:12 +0800

    need to Role caidan

--- service/service-acl/src/main/java/com/atguigu/ssyx/acl/controller/RoleController.java
+++ service/service-acl/src/main/java/com/atguigu/ssyx/acl/controller/RoleController.java
@@ -8,72 +8,81 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-/**
- * 角色管理
- */
+@Api(tags = "角色接口")
 @RestController
 @RequestMapping("/admin/acl/role")
-@Api(tags = "用户管理")
-@Slf4j
+//@CrossOrigin //跨域
 public class RoleController {
 
+    //注入service
     @Autowired
     private RoleService roleService;
 
-    @ApiOperation(value = "获取角色分页列表")
-    @GetMapping("{page}/{limit}")
-    public Result index(
-            @ApiParam(name = "page", value = "当前页码", required = true)
-            @PathVariable Long page,
+    //1 角色列表（条件分页查询）
+    @ApiOperation("角色条件分页查询")
+    @GetMapping("{current}/{limit}")
+    public Result pageList(@PathVariable Long current,
+                           @PathVariable Long limit,
+                           RoleQueryVo roleQueryVo) {
+        //1 创建page对象，传递当前页和每页记录数
+        // current：当前页
+        // limit: 每页显示记录数
+        Page<Role> pageParam = new Page<>(current,limit);
 
-            @ApiParam(name = "limit", value = "每页记录数", required = true)
-            @PathVariable Long limit,
+        //2 调用service方法实现条件分页查询，返回分页对象
+        IPage<Role> pageModel = roleService.selectRolePage(pageParam,roleQueryVo);
 
-            @ApiParam(name = "roleQueryVo", value = "查询对象", required = false)
-            RoleQueryVo roleQueryVo) {
-        Page<Role> pageParam = new Page<>(page, limit);
-        IPage<Role> pageModel = roleService.selectPage(pageParam, roleQueryVo);
         return Result.ok(pageModel);
     }
 
-    @ApiOperation(value = "获取角色")
+    //2 根据id查询角色
+    @ApiOperation("根据id查询角色")
     @GetMapping("get/{id}")
     public Result get(@PathVariable Long id) {
         Role role = roleService.getById(id);
         return Result.ok(role);
     }
 
-    @ApiOperation(value = "新增角色")
+    //3 添加角色
+    @ApiOperation("添加角色")
     @PostMapping("save")
     public Result save(@RequestBody Role role) {
-        roleService.save(role);
-        return Result.ok(null);
+        boolean is_success = roleService.save(role);
+        if(is_success) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
     }
 
-    @ApiOperation(value = "修改角色")
+    //4 修改角色
+    @ApiOperation("修改角色")
     @PutMapping("update")
-    public Result updateById(@RequestBody Role role) {
+    public Result update(@RequestBody Role role) {
         roleService.updateById(role);
         return Result.ok(null);
     }
 
-    @ApiOperation(value = "删除角色")
+    //5 根据id删除角色
+    @ApiOperation("根据id删除角色")
     @DeleteMapping("remove/{id}")
     public Result remove(@PathVariable Long id) {
         roleService.removeById(id);
         return Result.ok(null);
     }
 
-    @ApiOperation(value = "根据id列表删除角色")
+    //6 批量删除角色
+    // json数组[1,2,3]  --- java的list集合
+    @ApiOperation("批量删除角色")
     @DeleteMapping("batchRemove")
     public Result batchRemove(@RequestBody List<Long> idList) {
         roleService.removeByIds(idList);
         return Result.ok(null);
     }
+
 }
